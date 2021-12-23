471400a6eeeabd036f70e87d4ba8a215918cbb1c
elsaCapunta <j.vasquez0216@gmail.com>
2021-12-23 15:46:27 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,4 @@
 
- var firebaseConfig ={
-    apiKey: "AIzaSyC2k_ds-MNuNoHBb6hATa5giMMz54nXg-g",
-    authDomain: "cortatuweb-5dd32.firebaseapp.com",
-    projectId: "cortatuweb-5dd32",
-    storageBucket: "cortatuweb-5dd32.appspot.com",
-    messagingSenderId: "598711012010",
-    appId: "1:598711012010:web:64b7ef19a9e225b1085ed3"
-}
-
-firebase.initializeApp(firebaseConfig); 
 
 
 const auth = firebase.auth();
